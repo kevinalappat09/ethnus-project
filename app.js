a6d94a52b3cd6102ba141a78e1ff8fe3c72801b2
kevinalappat09 <35099148+kevinalappat09@users.noreply.github.com>
2024-07-07 10:28:38 +0530

    added start command

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(passport.session());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/tasks", require("./routes/tasks"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
 
 require("dotenv").config();
